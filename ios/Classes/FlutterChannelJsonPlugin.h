b72a58a8fd8f5b6f8c16cce69f08dd53d297a615
king <3925@winnermedical.com>
2022-04-02 11:11:52 +0800

    init

---
+++ ios/Classes/FlutterChannelJsonPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterChannelJsonPlugin : NSObject<FlutterPlugin>
+@end
---
+++ lib/flutter_channel_json.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+// class FlutterChannelJson {
+//   static const MethodChannel _channel =
+//       const MethodChannel('flutter_channel_json');
+
+//   static Future<String> get platformVersion async {
+//     final String version = await _channel.invokeMethod('getPlatformVersion');
+//     return version;
+//   }
+// }
